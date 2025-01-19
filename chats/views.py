b1015a343ebecb0df863a29c898a549dff329680
SAWANFURY <133782913+SAWANFURY@users.noreply.github.com>
2025-01-19 10:20:42 +0530

    Update views.py

--- chats/views.py
+++ chats/views.py
@@ -1,24 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth import get_user_model
 from chats.models import ChatModel
-# Create your views here.
-
 
 User = get_user_model()
 
 
+@login_required
 def index(request):
+    # Exclude the currently logged-in user
     users = User.objects.exclude(username=request.user.username)
     return render(request, 'index.html', context={'users': users})
 
 
+@login_required
 def chatPage(request, username):
-    user_obj = User.objects.get(username=username)
+    # Safely get the user object or return a 404
+    user_obj = get_object_or_404(User, username=username)
+
+    # Exclude the currently logged-in user
     users = User.objects.exclude(username=request.user.username)
 
-    if request.user.id > user_obj.id:
-        thread_name = f'chat_{request.user.id}-{user_obj.id}'
+    # Compare user IDs only if both are not None
+    if request.user.id and user_obj.id:
+        if request.user.id > user_obj.id:
+            thread_name = f'chat_{request.user.id}-{user_obj.id}'
+        else:
+            thread_name = f'chat_{user_obj.id}-{request.user.id}'
+        
+        # Fetch the messages for the thread
+        message_objs = ChatModel.objects.filter(thread_name=thread_name)
     else:
-        thread_name = f'chat_{user_obj.id}-{request.user.id}'
-    message_objs = ChatModel.objects.filter(thread_name=thread_name)
-    return render(request, 'main_chat.html', context={'user': user_obj, 'users': users, 'messages': message_objs})
+        # Handle case where user IDs are invalid
+        message_objs = []
+
+    return render(request, 'main_chat.html', context={
+        'user': user_obj,
+        'users': users,
+        'messages': message_objs
+    })
